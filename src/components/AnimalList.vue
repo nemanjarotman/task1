0bbeeb428b324dcf2b29e912c85a58862e66cf9b
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2019-04-27 19:26:24 +0200

    Zadatak 1. Dodat data.json file kao baza za zivotinje. Dodato malo css-a i citanje baze u fajlu Animal.vue

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="animal-list">
+        <h1>All Animals</h1>
+            <div v-for="animal in animals"
+            :key="animal.id" class="animals"><p class="zivotinje">{{ animal.ime }} | {{ animal.vrsta }} | {{ animal.datum }}</p></div>
+    </div>
+</template>
+
+
+<script>
+
+import json from '../data.json'
+
+export default{
+    data(){
+        return{
+            animals: json
+        }
+    },
+    methods: {
+
+    },
+    
+}
+
+</script>
+
+<style>
+.zivotinje{
+    box-sizing: border-box;
+    text-align: left;
+    font-size: 200%;
+    padding: 20px;
+    background: rgb(144, 241, 144);
+    font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    color: blue;
+}
+</style>
