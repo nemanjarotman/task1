62bcd76284e798db16138048c9c9807ad0349acf
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2019-05-02 20:28:18 +0200

    Zadatak 6 editovan. Ne prikazuju se sektori za pocetne zivotinje koje su uvezene iz json fajla. Za svaku novu zivotinju se kreira i njen sektor. Dodata lista sektora iz 7. zadatka sa css-om. Potrebno jos oraditi funkcionalsnost da ne vraca undefiend u alert windowu.

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -5,21 +5,26 @@
             <p>Vrsta: <input v-model="animal.vrsta" placeholder="Vrsta"></p>
             <p>Datum: <input v-model="animal.datum" placeholder="Datum"></p>
             <label>Sektori</label>
-            <select v-model="sector">
+            <select v-model="animal.sector">
                 <option v-for="sector in sectors" :key="sector.id">{{ sector }}</option>    
             </select> 
                 <button class="addanimal" v-on:click.prevent="addAnimal">Add Animal</button>
-        </form>
+        </form>        
         
         <h1>All Animals</h1>
             <div v-for="(animal, index) in animals"
                 :key="animal.id" class="animals">
-                <p class="zivotinje">{{ animal.ime }} | {{ animal.vrsta }} | {{ animal.datum | nepoznat }} | {{sector}}
+                <p class="zivotinje">{{ animal.ime }} | {{ animal.vrsta }} | {{ animal.datum | nepoznat }} | {{animal.sector}}
                     <button v-on:click = "removeElement(index)">Remove</button>
                     <button v-on:click = "moveToTop(animal,index)">Move To Top</button>
                 </p>
             </div>
-
+        <h1>All Sektors</h1>    
+            <div v-for="(sector, animal) in sectors" :key="sector.id" class="sectors">
+                <p class="sektori">{{sector}}
+                    <button v-on:click = "vidiListuZivotinja(animal.sector)">Vidi listu zivotinja</button>
+                </p>
+            </div>
     </div>
 </template>
 
@@ -51,13 +56,16 @@ export default{
                 ime: this.animal.ime,
                 vrsta: this.animal.vrsta,
                 datum: this.animal.datum,
-            };
+                sector: this.animal.sector
+            }
             this.animals.push(newAnimal).then(function(data){
                 this.submitted =true;
             });
         },
-    },
-    
+        vidiListuZivotinja(value){
+            alert(value);
+        }
+    }
 }
 
 </script>
@@ -73,6 +81,15 @@ export default{
     color: blue;
 }
 
+.sektori{
+    box-sizing: border-box;
+    text-align: left;
+    font-size: 200%;
+    padding: 20px;
+    background: #ff1a1a;
+    color: #333;
+}
+
 .addanimal{
     width:150px;
 }
@@ -86,4 +103,5 @@ button{
     background-color: #333;
     color: #ff0000;
 }
+
 </style>
